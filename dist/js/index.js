025b291e1c209098f5724e64e578f806b3d5ad37
Izabella Loyse <izabellaloyse13@gmail.com>
2022-01-14 23:27:02 -0300

    feat: add as funcionalidades do TS nos componentes

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,18 @@
 "use strict";
-const greeting = (name) => {
-    return `Hello ${name}`;
+const button = document.querySelector('.js-button-calculate');
+const getToPixelValues = (valuePixels, valueRoot) => {
+    const result = valuePixels / valueRoot;
+    return result;
 };
-console.log(greeting("John"));
+const getValuesToInputPixels = () => {
+    const rootInput = document.querySelector('#js-input-root');
+    const pixelsInput = document.querySelector('#js-input-pixels');
+    const rootInputValue = Number(rootInput.value);
+    const pixelsInputValue = Number(pixelsInput.value);
+    return getToPixelValues(pixelsInputValue, rootInputValue);
+};
+const resultValueREM = () => {
+    const remInput = document.querySelector('#js-input-rem');
+    remInput.value = getValuesToInputPixels().toString();
+};
+button.addEventListener('click', resultValueREM);
---
+++ src/ts/index.ts
@@ -0,0 +1,32 @@
+const button: HTMLButtonElement = document.querySelector(
+  '.js-button-calculate',
+) as HTMLButtonElement;
+
+const getToPixelValues = (valuePixels: number, valueRoot: number) => {
+  const result = valuePixels / valueRoot;
+  return result;
+};
+
+const getValuesToInputPixels = () => {
+  const rootInput: HTMLInputElement = document.querySelector(
+    '#js-input-root',
+  ) as HTMLInputElement;
+  const pixelsInput: HTMLInputElement = document.querySelector(
+    '#js-input-pixels',
+  ) as HTMLInputElement;
+
+  const rootInputValue: number = Number(rootInput.value);
+  const pixelsInputValue: number = Number(pixelsInput.value);
+
+  return getToPixelValues(pixelsInputValue, rootInputValue);
+};
+
+const resultValueREM = () => {
+  const remInput: HTMLInputElement = document.querySelector(
+    '#js-input-rem',
+  ) as HTMLInputElement;
+
+  remInput.value = getValuesToInputPixels().toString();
+};
+
+button.addEventListener('click', resultValueREM);
